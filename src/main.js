a1e5438490c11eb0ae9d288f49d3d4c067598680
Keshav-CUJ <ke23becse26@cujammu.ac.in>
2026-02-22 20:01:43 +0530

    Delete duplicate files

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from './App.vue'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'  //for css and styling
-import router from './router'
-
-
-createApp(App).use(router).mount('#app');
